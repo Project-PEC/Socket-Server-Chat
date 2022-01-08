f43994031e52ade2765051d0783cb83f08f2940a
Arsh Kashyap <arshk102001@gmail.com>
2022-01-08 17:27:55 +0530

    temp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(process.env.PORT||8900, {
     cors: {
-        origin: "https://coinxchange.herokuapp.com/"
+        origin: "http://localhost:3000"
     }
 });
 let users = []
